242ad30c7e0a689bf31b7354168bb517d48ce357
Vanessaeletron <vanessa.nobrega@outlook.com>
2018-03-14 17:40:37 -0300

    Update ola_usuario_1.c

--- 1_Respostas/ola_usuario_1.c
+++ 1_Respostas/ola_usuario_1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-main(){
+int main(){
   char nome[255];
   
 print("Qual o seu nome? \n");
 scanf ("%s", &nome);
 
-printf("Olá "%s",nome);
+printf("Olá %s",nome);
 return 0;
 }
