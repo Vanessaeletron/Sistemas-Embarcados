2ba7acc2189e4c5d0687eeb7186f6b8636d84e2f
Vanessaeletron <vanessa.nobrega@outlook.com>
2018-04-08 21:48:57 -0300

    Create Ola_usuario3.c

---
+++ 1_Respostas/3_GCC/Ola_usuario3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, const char *argv[ ]){
+printf("Olá %s \n", argv[1]);
+printf("Nº de argumentos: %d \n", argc);
+for (i=1; i<argc, i++){
+printf ("%s", argv[i]);}
+return 0;
+}
