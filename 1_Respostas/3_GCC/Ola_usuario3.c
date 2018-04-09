f782ccb7969d7265d1fa0f2702adc99b9e2000e8
Vanessaeletron <vanessa.nobrega@outlook.com>
2018-04-08 22:12:19 -0300

    Update Ola_usuario3.c

--- 1_Respostas/3_GCC/Ola_usuario3.c
+++ 1_Respostas/3_GCC/Ola_usuario3.c
@@ -5,7 +5,7 @@ int i;
 int main (int argc, const char *argv[ ]){
 printf("Olá %s \n", argv[1]);
 printf("Nº de argumentos: %d \n", argc);
-for (i=1; i<argc, i++){
+for (i=1; i<argc, i++);{
 printf ("%s", argv[i]);}
 return 0;
 }
