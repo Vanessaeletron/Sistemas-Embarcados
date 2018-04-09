16219bc4e742004144760ab19717747631e3b862
Vanessaeletron <vanessa.nobrega@outlook.com>
2018-04-08 21:26:58 -0300

    Update Ola_usuario2.c

--- 1_Respostas/3_GCC/Ola_usuario2.c
+++ 1_Respostas/3_GCC/Ola_usuario2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[ ]){
+int main (int argc, const char *argv[ ]){
 printf("Olá %s \n", argv[1]);
 printf(Nº de argumentos: %d \n, argc);
 return 0;
